7d57be932af5af78c82ba08a44d3a79336fbebc9
Hone682 <hjgazzx00@gmail.com>
2023-04-27 10:32:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/implicit/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.implicit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import  android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] PERMISSIONS ={Manifest.permission.CALL_PHONE, Manifest.permission.CAMERA};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int permission = ContextCompat.checkSelfPermission(this, PERMISSIONS[0]);
+            int permission1 = ContextCompat.checkSelfPermission(this, PERMISSIONS[1]);
+            if ((permission != PackageManager.PERMISSION_GRANTED)
+                    || (permission1 != PackageManager.PERMISSION_GRANTED)) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, 1);}}}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1){
+            if(grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                Button btnCall =findViewById(R.id.btnCall);
+                btnCall.setEnabled(false);
+
+            }
+
+            if(grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                Button btnCamera =findViewById(R.id.btnCamera);
+                btnCamera.setEnabled(false);
+
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if((requestCode == 1) && (resultCode==RESULT_OK)){
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iVCamera = findViewById(R.id.iVCamera);
+            iVCamera.setImageBitmap(bitmap);
+        }
+    }
+
+    public void onClick(View view){
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.btnCall:
+                intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:01032405725"));
+                break;
+            case R.id.btnDial:
+                intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01032405725"));
+                break;
+            case R.id.btnSms:
+                intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:01032405725"));
+                intent.putExtra("sms_body", "스마트모바일프로그래밍 리포트 다 하셨나요?");
+                break;
+            case R.id.btnMap:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.30, 127.2?z=10"));
+                break;
+            case R.id.btnWeb:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                break;
+            case R.id.btnContact:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people/"));
+                break;
+
+            case R.id.btnCamera:
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                break;
+
+
+        }
+        if(view.getId()==R.id.btnCamera)
+            startActivityForResult(intent,1);
+        else
+            startActivity(intent);
+
+    }
+}
+
